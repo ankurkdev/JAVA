44df92bb747db26bdfffa656d161209a6af9f943
ankurkdev <ankur.k.dev@gmail.com>
2026-03-07 22:44:50 +0530

    initial commit

---
+++ FirstCode.java
@@ -0,0 +1,10 @@
+class FirstCode{
+    public static void main(String[] args){
+        System.out.println("\nHello Ankur\n");
+        
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
