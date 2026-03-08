aa71c0c58caa219d67618b07a69e69e0fe4905cd
ankurkdev <ankur.k.dev@gmail.com>
2026-03-08 19:55:48 +0530

    Only input Function

---
+++ Input.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Input{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        // next() is used to read the next token of input "like a word ankur Kushwaha print only ankur not Kushwaha"
+        // nextLine() is used to read the next line of input "like a sentence ankur Kushwaha print whole sentence"
+        // nextInt() is used to read the next integer input "like a number 12345 print only 12345"
+        // nextDouble() is used to read the next double input "like a number 12345.6789 print only 12345.6789"
+        // nextFloat() is used to read the next float input "like a number 12345.6789f print only 12345.6789f"
+        // nextLong() is used to read the next long input "like a number 12345678901234567890 print only 12345678901234567890"
+        // nextBoolean() is used to read the next boolean input "like a boolean true or false print only true or false"
+        // nextByte() is used to read the next byte input "like a byte 123 print only 123"
+        // nextShort() is used to read the next short input "like a short 12345 print only 12345"
+        // nextChar() is used to read the next character input "like a character a print only a"
+        System.out.println(name);
+        
+    }
+}
