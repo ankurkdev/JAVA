b75e1440b01749c8020d4a4dc63096c7a1d90136
ankurkdev <ankur.k.dev@gmail.com>
2026-03-09 22:01:38 +0530

    Print My First code using operators

---
+++ variables.java
@@ -0,0 +1,10 @@
+class variables{
+    public static void main(String [] args){
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+        int mul = a*b;
+        System.out.println(sum);
+        System.out.println(mul);
+    }
+}
